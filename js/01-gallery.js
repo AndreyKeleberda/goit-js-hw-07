93b08663361242bbdbc5b76adb47fa017bba1d07
AndreyKeleberda <a.keleberda@ukr.net>
2022-02-07 20:44:48 +0200

    1st task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
+// galleryItems.map((item) => {
+//   console.log(item)
+//   const template = `<div class="gallery__item">
+//   <a class="gallery__link" href="large-image.jpg">
+//     <img
+//       class="gallery__image"
+//       src=${item.preview}
+//       data-source=${item.original}
+//       alt=${item.description}
+//     />
+//   </a>
+// </div>`
+// return template
+// })
+const galleryWrapper = document.querySelector('.gallery')
+const markUp = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+      <img
+        class="gallery__image"
+        src=${preview}
+        data-source=${original}
+        alt=${description}
+      />
+    </a>
+  </div>`,
+  )
+  .join('')
+
+galleryWrapper.insertAdjacentHTML('afterbegin', markUp)
+
+galleryWrapper.addEventListener('click', (event) => {
+  event.preventDefault()
+  const instance = basicLightbox.create(`
+  <div class="modal">
+      <img  src=${event.target.dataset.source} alt='image' class = "modalImage" />
+  </div>
+  `)
+  instance.show()
+  window.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+      instance.close()
+    }
+  })
+  document.querySelector('.modal').addEventListener('click', (event) => {
+    if (event.target.nodeName !== 'IMG') {
+      instance.close()
+    }
+  })
+})
+
 console.log(galleryItems)
